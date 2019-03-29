8f6008d0e61354f60fbe1d6cf12890e89d6101c4
leslie.alldridge <leslie.alldridge@gmail.com>
2019-03-30 09:07:14 +1300

    patch

--- users_test/test_users.py
+++ users_test/test_users.py
@@ -1,11 +1,16 @@
 import unittest
 from users import get_users
+from unittest.mock import patch
 
 
 class BasicTests(unittest.TestCase):
-    def test_request_response(self):
+    @patch('users.requests.get')  # Mock 'requests' module 'get' method.
+    def test_request_response_with_decorator(self, mock_get):
+        """Mocking using a decorator"""
+        mock_get.return_value.status_code = 200  # Mock status code of response.
         response = get_users()
-        # Assert that the request-response cycle completed successfully with status code 200.
+
+        # Assert that the request-response cycle completed successfully.
         self.assertEqual(response.status_code, 200)
 
 
