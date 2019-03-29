0fb0aa6945cdbf351185ce731e7f48792025fa50
leslie.alldridge <leslie.alldridge@gmail.com>
2019-03-30 09:11:43 +1300

    mock json res

--- users_test/test_users.py
+++ users_test/test_users.py
@@ -6,12 +6,22 @@ from unittest.mock import patch
 class BasicTests(unittest.TestCase):
     @patch('users.requests.get')  # Mock 'requests' module 'get' method.
     def test_request_response_with_decorator(self, mock_get):
-        """Mocking using a decorator"""
-        mock_get.return_value.status_code = 200  # Mock status code of response.
+
+        mockuser = [{
+            "id": 0,
+            "first_name": "Dell",
+            "last_name": "Norval",
+            "phone": "994-979-3976"
+        }]
+
+        # Mock status code of response.
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = mockuser
         response = get_users()
 
         # Assert that the request-response cycle completed successfully.
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), mockuser)
 
 
 if __name__ == "__main__":
